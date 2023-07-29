548a10a1ff628aec7c06e22dcf145d89e62482cf
Joshua-Frazier <joshua.frazier1519@gmail.com>
2023-07-29 11:39:57 -0600

    Changed scraping module to puppeteer

--- src/utils/kslScraper.js
+++ src/utils/kslScraper.js
@@ -1,74 +1,79 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const puppeteer = require('puppeteer');
 const carService = require("../services/car-serv");
 
-// Define the base URL for the KSL search.
-const BASE_URL = "https://www.ksl.com/auto/search/index?";
+(async () => {
+  // Use dynamic import for 'p-retry'.
+  const pRetry = await import('p-retry');
 
-// Define a function to build a search URL given some parameters.
-function buildSearchUrl(zip, miles, make, model, year, price) {
-  return `${BASE_URL}zip=${zip}&miles=${miles}&make[]=${make}&model[]=${model}&year=${year}&priceTo=${price}&sellerType=Private&sort=0&perPage=96`;
-}
+  const BASE_URL = 'https://cars.ksl.com/search/';
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+  async function scrapeKsl(searchZipCode, searchRadius) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-async function scrapeKsl(url) {
-  console.log(`Scraping URL: ${url}`); // Added logging
+    await page.goto(BASE_URL, { waitUntil: 'networkidle0' });
 
-  try {
-    // Delay between 5 to 10 seconds (adjust as necessary)
-    await sleep(Math.random() * 5000 + 5000);
+    // Click on the 'Select Location' button
+    await page.click('.Location__Link-sc-73vzho-0');
 
-    const response = await axios.get(url, {
-      headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537' },
-      timeout: 10000
+    // Wait for the modal to appear and fill in the location and radius
+    await page.waitForSelector('.Modal__ModalMain-cfe9l3-0');
+    await page.waitForSelector('.Input__HTMLInput-sc-5nc449-2');
+    await page.focus('.Input__HTMLInput-sc-5nc449-2');
+    await page.type('.Input__HTMLInput-sc-5nc449-2', searchZipCode);
+
+    // Adjust the radius slider
+    const radiusValue = (100 - searchRadius) / 100;
+    await page.evaluate((value) => {
+      let slider = document.querySelector('.SingleSlider__SliderContainer-whdv95-0');
+      let event = new Event('input', { bubbles: true });
+      slider.value = value;
+      slider.dispatchEvent(event);
+    }, radiusValue);
+
+    // Submit the form
+    await page.keyboard.press('Enter');
+
+    // Wait for the results to load
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+    const carsList = await page.evaluate(() => {
+      const cars = Array.from(document.querySelectorAll('.listing'));
+
+      return cars.map(car => {
+        const titleElement = car.querySelector('.title');
+        const priceElement = car.querySelector('.price');
+        const detailsElement = car.querySelector('.details');
+
+        const yearMakeModel = titleElement.textContent.split(' ');
+        const year = yearMakeModel[0];
+        const make = yearMakeModel[1];
+        const model = yearMakeModel.slice(2).join(' ');
+        const title = `${year} ${make} ${model}`;
+        const price = priceElement.textContent;
+        const details = detailsElement.textContent;
+
+        return { title, price, year, make, model, details };
+      });
     });
 
-    if(response.status !== 200){
-      console.log(`Request to ${url} failed with status: ${response.status}`);
-      return;
-    }
-
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const carsList = [];
-
-    $(".listing").each(function () {
-      const title = $(this).find(".title").text();
-      const price = $(this).find(".price").text();
-      const yearMakeModel = $(this).find(".title > span").text().split(" ");
-      const year = yearMakeModel[0];
-      const make = yearMakeModel[1];
-      const model = yearMakeModel[2];
-      const details = $(this).find(".details").text();
-
-      carsList.push({ title, price, year, make, model, details });
-    });
-
-    if(carsList.length === 0){
-      console.log('No cars were found');
-    }
+    await browser.close();
 
     // After retrieving all cars, store them in your database
     for (const car of carsList) {
       console.log(car);
       await carService.createCar(car);
     }
-  } catch (error) {
-    console.error(`An error occurred while scraping: ${error}`);
   }
-}
 
+  // Use the function to search all cars within a 25-mile radius of the given zip code
+  pRetry.default(() => scrapeKsl("84020", 25), {
+    retries: 0,
+    onFailedAttempt: (error) => {
+      console.log(`Attempt ${error.attemptNumber} failed. There are ${error.retriesLeft} retries left.`);
+    }
+  }).catch((err) => {
+    console.error('Scraping failed:', err);
+  });
 
-// Use the function to build a search URL and scrape the page.
-const searchUrl = buildSearchUrl(
-  "84020",
-  "",
-  "Toyota",
-  "Camry",
-  "",
-  ""
-);
-scrapeKsl(searchUrl);
+})();
